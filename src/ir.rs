b675e0519f384b29d31f9b90f6d1427b1cdd7d5d
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-06-02 16:18:15 -0400

    ir: simple serialization (wip)

--- src/ir.rs
+++ src/ir.rs
@@ -5,10 +5,55 @@
 /// This type restricts the maximum width that a bit-vector type is allowed to have in our IR.
 pub type WidthInt = u32;
 
+/// This restricts the maximum value that a bit-vector literal can carry.
+pub type BVLiteralInt = u64;
+
 /// IR Nodes are only valid with their context
 pub trait Context {
-    fn add();
-    fn get_string(reference: StringRef) -> String;
+    // helper functions to construct expressions
+    fn bv_literal(&mut self, value: BVLiteralInt, width: WidthInt) -> BVExprRef {
+        self.add_bv_expr(BVExpr::Literal {value, width})
+    }
+    fn bv_symbol(&mut self, name: &str, width: WidthInt) -> BVExprRef {
+        let name_ref = self.add_string(name.to_string());
+        self.add_bv_expr(BVExpr::Symbol {name: name_ref, width })
+    }
+
+    // basic functions that need to be implemented by every context
+    fn add_string(&mut self, value: String) -> StringRef;
+    fn get_string(&self, reference: StringRef) -> String;
+    fn add_bv_expr(&mut self, value: BVExpr) -> BVExprRef;
+    fn get_bv_expr(&self, reference: BVExprRef) -> BVExpr;
+
+}
+
+#[derive(Default)]
+/// simple implementation of a context
+struct BasicContext {
+    strings: Vec<String>,
+    bv_exprs: Vec<BVExpr>,
+}
+
+impl Context for BasicContext {
+    fn add_string(&mut self, value: String) -> StringRef {
+        let index = self.strings.len();
+        self.strings.push(value);
+        StringRef(index as u16)
+    }
+
+    fn get_string(&self, reference: StringRef) -> String {
+        self.strings[reference.0 as usize].clone()
+    }
+
+    fn add_bv_expr(&mut self, value: BVExpr) -> BVExprRef {
+        let index = self.bv_exprs.len();
+        self.bv_exprs.push(value);
+        BVExprRef(index as u32)
+    }
+
+    fn get_bv_expr(&self, reference: BVExprRef) -> BVExpr {
+        self.bv_exprs[reference.0 as usize].clone()
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -31,7 +76,7 @@ pub enum BVExpr {
     // nullary
     Symbol{ name : StringRef, width: WidthInt},
     // TODO: support literals that do not fit into 64-bit
-    Literal { value: u64, width: WidthInt},
+    Literal { value: BVLiteralInt, width: WidthInt},
     // unary operations
     ZeroExt { e: BVExprRef, by: WidthInt },
     SignExt { e: BVExprRef, by: WidthInt },
@@ -82,6 +127,48 @@ pub enum ArrayExpr {
     Ite { cond: BVExprRef, tru: ArrayExprRef, fals: ArrayExprRef },
 }
 
+/// Serialize Expression to custom format
+fn serialize_bv_expr<C: Context>(ctx: &C, expr: BVExpr) -> String {
+    match expr {
+        BVExpr::Symbol { name, .. } => ctx.get_string(name),
+        BVExpr::Literal { value, width } =>
+            if width <= 8 { format!("{width}'b{value:b}") } else { format!("{width}'x{value:x}") },
+        BVExpr::ZeroExt { .. } => format!(""),
+        BVExpr::SignExt { .. } => format!(""),
+        BVExpr::Slice { .. } => format!(""),
+        BVExpr::Not(_) => format!(""),
+        BVExpr::Negate(_) => format!(""),
+        BVExpr::ReduceOr(_) => format!(""),
+        BVExpr::ReduceAnd(_) => format!(""),
+        BVExpr::ReduceXor(_) => format!(""),
+        BVExpr::Equal(_, _) => format!(""),
+        BVExpr::Implies(_, _) => format!(""),
+        BVExpr::Greater(_, _) => format!(""),
+        BVExpr::GreaterEqual(_, _) => format!(""),
+        BVExpr::Concat(_, _) => format!(""),
+        BVExpr::And(_, _) => format!(""),
+        BVExpr::Or(_, _) => format!(""),
+        BVExpr::Xor(_, _) => format!(""),
+        BVExpr::ShiftLeft(_, _) => format!(""),
+        BVExpr::ArithmeticShiftRight(_, _) => format!(""),
+        BVExpr::ShiftRight(_, _) => format!(""),
+        BVExpr::Add(_, _) => format!(""),
+        BVExpr::Mul(_, _) => format!(""),
+        BVExpr::SignedDiv(_, _) => format!(""),
+        BVExpr::UnsignedDiv(_, _) => format!(""),
+        BVExpr::SignedMod(_, _) => format!(""),
+        BVExpr::SignedRem(_, _) => format!(""),
+        BVExpr::UnsignedRem(_, _) => format!(""),
+        BVExpr::Sub(_, _) => format!(""),
+        BVExpr::ArrayRead { .. } => format!(""),
+        BVExpr::Ite { .. } => format!(""),
+    }
+}
+
+fn serialize_bv_expr_ref<C: Context>(ctx: &C, reference: BVExprRef) -> String {
+    serialize_bv_expr(ctx, ctx.get_bv_expr(reference))
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -96,5 +183,12 @@ mod tests {
         assert_eq!(std::mem::size_of::<BVExpr>(), 16);
         // 4 bytes for the tag, 4 * 3 bytes for the largest field
         assert_eq!(std::mem::size_of::<ArrayExpr>(), 16);
+    }
+
+    #[test]
+    fn simple_serialization() {
+        let mut ctx = BasicContext::default();
+        let test_expr = ctx.bv_symbol("test", 3);
+        assert_eq!("test", serialize_bv_expr_ref(&ctx, test_expr));
     }
 }
