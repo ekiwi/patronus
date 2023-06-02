9ab97ce2f462ad406b8699694331b3f9d395e3ec
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-06-02 14:01:25 -0400

    wip ir

--- src/ir.rs
+++ src/ir.rs
@@ -3,18 +3,26 @@
 // author: Kevin Laeufer <laeufer@berkeley.edu>
 
 /// IR Nodes are only valid with their context
-trait Context {
+trait Context<'a> {
     fn add();
     fn get_width(reference: WidthRef) -> u64;
-    fn get_string(reference: StringRef) -> &str;
+    fn get_string(reference: StringRef) -> &'a str;
 }
 
 struct StringRef(u16);
 struct WidthRef(u16);
+struct BVExprRef(u32);
+
+
 
 
 enum BVExpr {
-    Symbol(StringRef, WidthRef)
+    Symbol(StringRef, WidthRef),
+    // unary operations
+    
+
+    // binary operations
+    Add(BVExprRef, BVExprRef)
 }
 
 impl BVExpr {
@@ -23,3 +31,16 @@ impl BVExpr {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ir_type_size() {
+        assert_eq!(std::mem::size_of::<StringRef>(), 2);
+        assert_eq!(std::mem::size_of::<WidthRef>(), 2);
+        // 4 bytes for the tag, 8 bytes for the largest field
+        assert_eq!(std::mem::size_of::<BVExpr>(), 12);
+    }
+}
