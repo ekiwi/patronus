71e2cf331a8f935c85719f8e3144e45a9a8070b9
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-08-24 11:49:19 -0400

    remove template code

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,3 @@
 
 pub mod btor2;
 pub mod ir;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
