95004b094d09605ef73f05ac36b8570caa4ed0e1
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-08-28 14:48:29 -0400

    ir: unify BV and Array expressions

--- src/ir.rs
+++ src/ir.rs
@@ -21,14 +21,14 @@ pub trait GetNode<D: ?Sized, I: Clone + Copy> {
 }
 
 /// Convenience methods to construct IR nodes.
-pub trait NodeConstruction: AddNode<String, StringRef> + AddNode<BVExpr, BVExprRef> {
+pub trait NodeConstruction: AddNode<String, StringRef> + AddNode<Expr, ExprRef> {
     // helper functions to construct expressions
-    fn bv_literal(&mut self, value: BVLiteralInt, width: WidthInt) -> BVExprRef {
-        self.add(BVExpr::Literal { value, width })
+    fn bv_literal(&mut self, value: BVLiteralInt, width: WidthInt) -> ExprRef {
+        self.add(Expr::BVLiteral { value, width })
     }
-    fn bv_symbol(&mut self, name: &str, width: WidthInt) -> BVExprRef {
+    fn bv_symbol(&mut self, name: &str, width: WidthInt) -> ExprRef {
         let name_ref = self.add(name.to_string());
-        self.add(BVExpr::Symbol {
+        self.add(Expr::BVSymbol {
             name: name_ref,
             width,
         })
@@ -42,14 +42,14 @@ type StringInternerU16 = string_interner::StringInterner<
 /// The actual context implementation.
 struct Context {
     strings: StringInternerU16,
-    bv_exprs: indexmap::IndexSet<BVExpr>,
+    exprs: indexmap::IndexSet<Expr>,
 }
 
 impl Default for Context {
     fn default() -> Self {
         Context {
             strings: StringInternerU16::new(),
-            bv_exprs: indexmap::IndexSet::default(),
+            exprs: indexmap::IndexSet::default(),
         }
     }
 }
@@ -68,18 +68,18 @@ impl GetNode<str, StringRef> for Context {
     }
 }
 
-impl AddNode<BVExpr, BVExprRef> for Context {
-    fn add(&mut self, value: BVExpr) -> BVExprRef {
-        let (index, _) = self.bv_exprs.insert_full(value);
-        BVExprRef(index as u32)
+impl AddNode<Expr, ExprRef> for Context {
+    fn add(&mut self, value: Expr) -> ExprRef {
+        let (index, _) = self.exprs.insert_full(value);
+        ExprRef(index as u32)
     }
 }
 
-impl GetNode<BVExpr, BVExprRef> for Context {
-    fn get(&self, reference: BVExprRef) -> &BVExpr {
-        self.bv_exprs
+impl GetNode<Expr, ExprRef> for Context {
+    fn get(&self, reference: ExprRef) -> &Expr {
+        self.exprs
             .get_index(reference.0 as usize)
-            .expect("Invalid BVExprRef!")
+            .expect("Invalid ExprRef!")
     }
 }
 
@@ -88,109 +88,99 @@ impl NodeConstruction for Context {}
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct StringRef(string_interner::symbol::SymbolU16);
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct BVExprRef(u32);
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub struct ArrayExprRef(u32);
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum STMExpr {
-    BitVec(BVExpr),
-}
+pub struct ExprRef(u32);
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
-/// Represents a SMT bit-vector expression.
-pub enum BVExpr {
+/// Represents a SMT bit-vector or array expression.
+pub enum Expr {
+    // Bit-Vector Expressions
     // nullary
-    Symbol {
+    BVSymbol {
         name: StringRef,
         width: WidthInt,
     },
     // TODO: support literals that do not fit into 64-bit
-    Literal {
+    BVLiteral {
         value: BVLiteralInt,
         width: WidthInt,
     },
     // unary operations
-    ZeroExt {
-        e: BVExprRef,
+    BVZeroExt {
+        e: ExprRef,
         by: WidthInt,
     },
-    SignExt {
-        e: BVExprRef,
+    BVSignExt {
+        e: ExprRef,
         by: WidthInt,
     },
-    Slice {
-        e: BVExprRef,
+    BVSlice {
+        e: ExprRef,
         hi: WidthInt,
         lo: WidthInt,
     },
-    Not(BVExprRef),
-    Negate(BVExprRef),
-    ReduceOr(BVExprRef),
-    ReduceAnd(BVExprRef),
-    ReduceXor(BVExprRef),
+    BVNot(ExprRef),
+    BVNegate(ExprRef),
+    BVReduceOr(ExprRef),
+    BVReduceAnd(ExprRef),
+    BVReduceXor(ExprRef),
     // binary operations
-    Equal(BVExprRef, BVExprRef),
-    Implies(BVExprRef, BVExprRef),
-    Greater(BVExprRef, BVExprRef),
-    GreaterSigned(BVExprRef, BVExprRef),
-    GreaterEqual(BVExprRef, BVExprRef),
-    GreaterEqualSigned(BVExprRef, BVExprRef),
-    Concat(BVExprRef, BVExprRef),
+    BVEqual(ExprRef, ExprRef),
+    BVImplies(ExprRef, ExprRef),
+    BVGreater(ExprRef, ExprRef),
+    BVGreaterSigned(ExprRef, ExprRef),
+    BVGreaterEqual(ExprRef, ExprRef),
+    BVGreaterEqualSigned(ExprRef, ExprRef),
+    BVConcat(ExprRef, ExprRef),
     // binary arithmetic
-    And(BVExprRef, BVExprRef),
-    Or(BVExprRef, BVExprRef),
-    Xor(BVExprRef, BVExprRef),
-    ShiftLeft(BVExprRef, BVExprRef),
-    ArithmeticShiftRight(BVExprRef, BVExprRef),
-    ShiftRight(BVExprRef, BVExprRef),
-    Add(BVExprRef, BVExprRef),
-    Mul(BVExprRef, BVExprRef),
-    SignedDiv(BVExprRef, BVExprRef),
-    UnsignedDiv(BVExprRef, BVExprRef),
-    SignedMod(BVExprRef, BVExprRef),
-    SignedRem(BVExprRef, BVExprRef),
-    UnsignedRem(BVExprRef, BVExprRef),
-    Sub(BVExprRef, BVExprRef),
+    BVAnd(ExprRef, ExprRef),
+    BVOr(ExprRef, ExprRef),
+    BVXor(ExprRef, ExprRef),
+    BVShiftLeft(ExprRef, ExprRef),
+    BVArithmeticShiftRight(ExprRef, ExprRef),
+    BVShiftRight(ExprRef, ExprRef),
+    BVAdd(ExprRef, ExprRef),
+    BVMul(ExprRef, ExprRef),
+    BVSignedDiv(ExprRef, ExprRef),
+    BVUnsignedDiv(ExprRef, ExprRef),
+    BVSignedMod(ExprRef, ExprRef),
+    BVSignedRem(ExprRef, ExprRef),
+    BVUnsignedRem(ExprRef, ExprRef),
+    BVSub(ExprRef, ExprRef),
     //
-    ArrayRead {
-        array: ArrayExprRef,
-        index: BVExprRef,
+    BVArrayRead {
+        array: ExprRef,
+        index: ExprRef,
     },
     // ternary op
-    Ite {
-        cond: BVExprRef,
-        tru: BVExprRef,
-        fals: BVExprRef,
+    BVIte {
+        cond: ExprRef,
+        tru: ExprRef,
+        fals: ExprRef,
     },
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-/// Represents a SMT array expression.
-pub enum ArrayExpr {
+    // Array Expressions
     // nullary
-    Symbol {
+    ArraySymbol {
         name: StringRef,
         index_width: WidthInt,
         data_width: WidthInt,
     },
     // unary
-    Constant {
-        e: BVExprRef,
+    ArrayConstant {
+        e: ExprRef,
         index_width: WidthInt,
     },
     // binary
-    Equal(ArrayExprRef, ArrayExprRef),
+    ArrayEqual(ExprRef, ExprRef),
     // ternary
-    Store {
-        array: ArrayExprRef,
-        index: BVExprRef,
-        data: BVExprRef,
+    ArrayStore {
+        array: ExprRef,
+        index: ExprRef,
+        data: ExprRef,
     },
-    Ite {
-        cond: BVExprRef,
-        tru: ArrayExprRef,
-        fals: ArrayExprRef,
+    ArrayIte {
+        cond: ExprRef,
+        tru: ExprRef,
+        fals: ExprRef,
     },
 }
 
@@ -199,31 +189,22 @@ pub enum SignalKind {
     Output,
 }
 
-pub enum Signal {
-    BV {
-        name: StringRef,
-        kind: SignalKind,
-        expr: BVExprRef,
-    },
+pub struct Signal {
+    name: StringRef,
+    kind: SignalKind,
+    expr: ExprRef,
 }
 
-pub enum State {
-    BV {
-        name: StringRef,
-        width: WidthInt,
-        init: Option<BVExprRef>,
-        next: Option<BVExprRef>,
-    },
-}
-
-pub enum Input {
-    BV { name: StringRef, width: WidthInt },
+pub struct State {
+    symbol: ExprRef,
+    init: Option<ExprRef>,
+    next: Option<ExprRef>,
 }
 
 pub struct TransitionSystem {
     name: StringRef,
     states: Vec<State>,
-    inputs: Vec<Input>,
+    inputs: Vec<ExprRef>,
     signals: Vec<Signal>,
 }
 
@@ -237,28 +218,28 @@ trait SerializableIrNode {
     }
 }
 
-impl SerializableIrNode for BVExpr {
+impl SerializableIrNode for Expr {
     fn serialize(&self, ctx: &Context, writer: &mut impl (std::io::Write)) -> std::io::Result<()> {
         match *self {
-            BVExpr::Symbol { name, .. } => write!(writer, "{}", ctx.get(name)),
-            BVExpr::Literal { value, width } => {
+            Expr::BVSymbol { name, .. } => write!(writer, "{}", ctx.get(name)),
+            Expr::BVLiteral { value, width } => {
                 if width <= 8 {
                     write!(writer, "{width}'b{value:b}")
                 } else {
                     write!(writer, "{width}'x{value:x}")
                 }
             }
-            BVExpr::ZeroExt { e, by } => {
+            Expr::BVZeroExt { e, by } => {
                 write!(writer, "zext(")?;
                 e.serialize(ctx, writer)?;
                 write!(writer, ", {by})")
             }
-            BVExpr::SignExt { e, by } => {
+            Expr::BVSignExt { e, by } => {
                 write!(writer, "sext(")?;
                 e.serialize(ctx, writer)?;
                 write!(writer, ", {by})")
             }
-            BVExpr::Slice { e, hi, lo } => {
+            Expr::BVSlice { e, hi, lo } => {
                 e.serialize(ctx, writer)?;
                 if hi == lo {
                     write!(writer, "[{hi}]")
@@ -266,180 +247,215 @@ impl SerializableIrNode for BVExpr {
                     write!(writer, "[{hi}:{lo}]")
                 }
             }
-            BVExpr::Not(e) => {
+            Expr::BVNot(e) => {
                 write!(writer, "not(")?;
                 e.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Negate(e) => {
+            Expr::BVNegate(e) => {
                 write!(writer, "neg(")?;
                 e.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::ReduceOr(e) => {
+            Expr::BVReduceOr(e) => {
                 write!(writer, "redor(")?;
                 e.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::ReduceAnd(e) => {
+            Expr::BVReduceAnd(e) => {
                 write!(writer, "redand(")?;
                 e.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::ReduceXor(e) => {
+            Expr::BVReduceXor(e) => {
                 write!(writer, "redxor(")?;
                 e.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Equal(a, b) => {
+            Expr::BVEqual(a, b) => {
                 write!(writer, "eq(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Implies(a, b) => {
+            Expr::BVImplies(a, b) => {
                 write!(writer, "implies(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Greater(a, b) => {
+            Expr::BVGreater(a, b) => {
                 write!(writer, "ugt(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::GreaterSigned(a, b) => {
+            Expr::BVGreaterSigned(a, b) => {
                 write!(writer, "sgt(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::GreaterEqual(a, b) => {
+            Expr::BVGreaterEqual(a, b) => {
                 write!(writer, "ugte(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::GreaterEqualSigned(a, b) => {
+            Expr::BVGreaterEqualSigned(a, b) => {
                 write!(writer, "sgte(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Concat(a, b) => {
+            Expr::BVConcat(a, b) => {
                 write!(writer, "concat(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::And(a, b) => {
+            Expr::BVAnd(a, b) => {
                 write!(writer, "and(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Or(a, b) => {
+            Expr::BVOr(a, b) => {
                 write!(writer, "or(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Xor(a, b) => {
+            Expr::BVXor(a, b) => {
                 write!(writer, "xor(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::ShiftLeft(a, b) => {
+            Expr::BVShiftLeft(a, b) => {
                 write!(writer, "logical_shift_left(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::ArithmeticShiftRight(a, b) => {
+            Expr::BVArithmeticShiftRight(a, b) => {
                 write!(writer, "arithmetic_shift_right(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::ShiftRight(a, b) => {
+            Expr::BVShiftRight(a, b) => {
                 write!(writer, "logical_shift_right(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Add(a, b) => {
+            Expr::BVAdd(a, b) => {
                 write!(writer, "add(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Mul(a, b) => {
+            Expr::BVMul(a, b) => {
                 write!(writer, "mul(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::SignedDiv(a, b) => {
+            Expr::BVSignedDiv(a, b) => {
                 write!(writer, "sdiv(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::UnsignedDiv(a, b) => {
+            Expr::BVUnsignedDiv(a, b) => {
                 write!(writer, "udiv(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::SignedMod(a, b) => {
+            Expr::BVSignedMod(a, b) => {
                 write!(writer, "smod(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::SignedRem(a, b) => {
+            Expr::BVSignedRem(a, b) => {
                 write!(writer, "srem(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::UnsignedRem(a, b) => {
+            Expr::BVUnsignedRem(a, b) => {
                 write!(writer, "urem(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::Sub(a, b) => {
+            Expr::BVSub(a, b) => {
                 write!(writer, "sub(")?;
                 a.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
                 b.serialize(ctx, writer)?;
                 write!(writer, ")")
             }
-            BVExpr::ArrayRead { .. } => write!(writer, "TODO: support array"),
-            BVExpr::Ite { cond, tru, fals } => {
+            Expr::BVArrayRead { array, index } => {
+                array.serialize(ctx, writer)?;
+                write!(writer, "[")?;
+                index.serialize(ctx, writer)?;
+                write!(writer, "]")
+            }
+            Expr::BVIte { cond, tru, fals } => {
+                write!(writer, "ite(")?;
+                cond.serialize(ctx, writer)?;
+                write!(writer, ", ")?;
+                tru.serialize(ctx, writer)?;
+                write!(writer, ", ")?;
+                fals.serialize(ctx, writer)?;
+                write!(writer, ")")
+            }
+            Expr::ArraySymbol { name, .. } => write!(writer, "{}", ctx.get(name)),
+            Expr::ArrayConstant { e, index_width } => {
+                write!(writer, "([")?;
+                e.serialize(ctx, writer)?;
+                write!(writer, "] x 2^{index_width})")
+            }
+            Expr::ArrayEqual(a, b) => {
+                write!(writer, "eq(")?;
+                a.serialize(ctx, writer)?;
+                write!(writer, ", ")?;
+                b.serialize(ctx, writer)?;
+                write!(writer, ")")
+            }
+            Expr::ArrayStore { array, index, data } => {
+                array.serialize(ctx, writer)?;
+                write!(writer, "[")?;
+                index.serialize(ctx, writer)?;
+                write!(writer, " := ")?;
+                data.serialize(ctx, writer)?;
+                write!(writer, "]")
+            }
+            Expr::ArrayIte { cond, tru, fals } => {
                 write!(writer, "ite(")?;
                 cond.serialize(ctx, writer)?;
                 write!(writer, ", ")?;
@@ -452,7 +468,7 @@ impl SerializableIrNode for BVExpr {
     }
 }
 
-impl SerializableIrNode for BVExprRef {
+impl SerializableIrNode for ExprRef {
     fn serialize(&self, ctx: &Context, writer: &mut impl (std::io::Write)) -> std::io::Result<()> {
         ctx.get(*self).serialize(ctx, writer)
     }
@@ -465,12 +481,10 @@ mod tests {
     #[test]
     fn ir_type_size() {
         assert_eq!(std::mem::size_of::<StringRef>(), 2);
-        assert_eq!(std::mem::size_of::<BVExprRef>(), 4);
+        assert_eq!(std::mem::size_of::<ExprRef>(), 4);
         assert_eq!(std::mem::size_of::<WidthInt>(), 4);
         // 4 bytes for the tag, 4 * 3 bytes for the largest field
-        assert_eq!(std::mem::size_of::<BVExpr>(), 16);
-        // 4 bytes for the tag, 4 * 3 bytes for the largest field
-        assert_eq!(std::mem::size_of::<ArrayExpr>(), 16);
+        assert_eq!(std::mem::size_of::<Expr>(), 16);
     }
 
     #[test]
