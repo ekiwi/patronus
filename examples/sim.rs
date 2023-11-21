717abfd4ca6fb17b0ba608a9cbd0486b46d7956e
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-11-20 20:00:09 -0500

    add wip simulator code

---
+++ examples/sim.rs
@@ -0,0 +1,33 @@
+// Copyright 2023 The Regents of the University of California
+// released under BSD 3-Clause License
+// author: Kevin Laeufer <laeufer@berkeley.edu>
+
+use clap::Parser;
+use libpatron::ir::*;
+use libpatron::*;
+use std::io::BufRead;
+
+#[derive(Parser, Debug)]
+#[command(name = "sim")]
+#[command(author = "Kevin Laeufer <laeufer@berkeley.edu>")]
+#[command(version)]
+#[command(about = "Simulates a transitions system from a btor2 file.", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    verbose: bool,
+    #[arg(long, help = "Filename of a testbench.")]
+    testbench: String,
+    #[arg(value_name = "BTOR2", index = 1)]
+    filename: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let (ctx, sys) = btor2::parse_file(&args.filename).expect("Failed to load btor2 file!");
+    if args.verbose {
+        println!("Loaded: {}", sys.name);
+        println!("{}", sys.serialize_to_str(&ctx));
+        println!();
+        println!();
+    }
+}
