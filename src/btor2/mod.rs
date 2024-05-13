22a76e6021840e68ffa04b74b8ab14716292faed
Kevin Läufer <laeufer@cs.berkeley.edu>
2024-05-13 15:58:14 -0400

    expose new load function

--- src/btor2/mod.rs
+++ src/btor2/mod.rs
@@ -5,6 +5,8 @@ mod parse;
 mod serialize;
 mod witness;
 
-pub use parse::{parse_file, parse_str, DEFAULT_INPUT_PREFIX, DEFAULT_STATE_PREFIX};
+pub use parse::{
+    parse_file, parse_file_with_ctx, parse_str, DEFAULT_INPUT_PREFIX, DEFAULT_STATE_PREFIX,
+};
 pub use serialize::{serialize, serialize_to_str};
 pub use witness::{parse_witness, parse_witnesses, print_witness, witness_to_string};
